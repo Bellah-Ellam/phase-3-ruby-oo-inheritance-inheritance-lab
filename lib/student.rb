05d171aed1b3137e29d2f3e5ec7322ef7291ac8f
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-05-24 00:04:51 +0300

    complete assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
-
-end
+class User
+    attr_accessor :first_name, :last_name
+  
+    def initialize
+      @first_name = ""
+      @last_name = ""
+    end
+  end
+  
+  class Student < User
+    attr_reader :knowledge
+  
+    def initialize
+      super
+      @knowledge = []
+    end
+  
+    def learn(knowledge_string)
+      @knowledge << knowledge_string
+    end
+  end
